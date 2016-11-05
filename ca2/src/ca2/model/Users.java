1063d54bec9ca152dd47a0875e18caf692fe87d1
voonlong <voonlong@gmail.com>
2016-11-05 17:06:28 +0800

    commit users file

---
+++ ca2/src/ca2/model/Users.java
@@ -0,0 +1,41 @@
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author andy
+ */
+@Entity
+public class Users implements Serializable{
+    
+     private static final long serialVersionUID = 1L;
+     
+     private String userid;
+     private String password;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+     
+     
+     
+}

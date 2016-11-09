7b37b2de62048cbcc2bd151ff8c7c2ad7ed8206f
Kevin <nkevinsst@gmail.com>
2016-11-09 20:33:07 +0800

    Enhancing view note

--- ca2/src/java/ca2/view/NoteView.java
+++ ca2/src/java/ca2/view/NoteView.java
@@ -6,11 +6,12 @@
 package ca2.view;
 
 import ca2.business.NoteBean;
-import ca2.business.UserBean;
 import ca2.model.Note;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -21,19 +22,20 @@ import javax.inject.Named;
 @RequestScoped
 public class NoteView {
     
-    private List<Note> noteList = null;
     @EJB private NoteBean noteBean;
+    @Inject private UserSession userSession;
+    
+    private List<Note> noteList = null;
+    
     public List<Note> getNoteList() {
         return noteList;
     }
-
     public void setNoteList(List<Note> noteList) {
         this.noteList = noteList;
-    }
+    } 
     
-    public String show(String userId){
-       noteList= noteBean.getNoteByUserId(userId);
-        return "showNote";
-    }
-    
+    @PostConstruct
+    public void init() {
+        noteList= noteBean.getNoteByUserId(userSession.getUsername());
+    }       
 }

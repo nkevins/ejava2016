8b31ee3b7ff148c0592eea87c7944c93662b798c
Kevin <nkevinsst@gmail.com>
2016-11-12 09:53:36 +0800

    Creating model

---
+++ epod/src/java/epod/model/Delivery.java
@@ -0,0 +1,76 @@
+package epod.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+
+@Entity
+public class Delivery implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="pkg_id")
+    private Integer packageId;
+    
+    private String name;
+    private String address;
+    private String phone;
+    
+    @Column(name="create_date")
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date createdDate;
+    
+    @OneToOne
+    @JoinColumn(name="pkg_id")
+    private Pod pod;
+
+    public Integer getPackageId() {
+        return packageId;
+    }
+    public void setPackageId(Integer packageId) {
+        this.packageId = packageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Pod getPod() {
+        return pod;
+    }
+    public void setPod(Pod pod) {
+        this.pod = pod;
+    }    
+    
+}
---
+++ epod/src/java/epod/model/Pod.java
@@ -0,0 +1,76 @@
+package epod.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+
+@Entity
+public class Pod implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="pod_id")
+    private Integer podId;
+    
+    private String note;
+    private byte[] image;
+    
+    @Column(name="delivery_date")
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date deliveryDate;
+    
+    @Column(name="ack_id")
+    private String ackId;
+    
+    @OneToOne(mappedBy="pod")
+    private Delivery delivery;
+
+    public Integer getPodId() {
+        return podId;
+    }
+    public void setPodId(Integer podId) {
+        this.podId = podId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getAckId() {
+        return ackId;
+    }
+    public void setAckId(String ackId) {
+        this.ackId = ackId;
+    }    
+
+    public Delivery getDelivery() {
+        return delivery;
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+    }   
+    
+}
